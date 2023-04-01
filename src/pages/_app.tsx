4e4a217e34a14dde87c0981aab4296bcf6b83fd4
charliehb1 <61828414+charliehb1@users.noreply.github.com>
2023-04-01 20:23:28 +0100

    feat: analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { type AppType } from 'next/dist/shared/lib/utils'
 import { Roboto_Mono } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 
 const roboto_mono = Roboto_Mono({
 	weight: ['400', '700'],
@@ -13,6 +14,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
 	return (
 		<main className={`${roboto_mono.className} bg-[#1E2124] text-white`}>
 			<Component {...pageProps} />
+			<Analytics />
 		</main>
 	)
 }
